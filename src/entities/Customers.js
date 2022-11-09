d0aa7ca60e9c4f4fb34660ced6a9077ee8560c48
Babacar Sadikh Mbodj <mbodjbabacarsadikh@outlook.fr>
2022-11-09 15:16:48 -0400

    task: adding the entity Customers

---
+++ src/entities/Customers.js
@@ -0,0 +1,8 @@
+module.exports = class Customer {
+    constructor(name, address, phone, email) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+        this.email = email;
+    }
+};
